546587311978b8070c72f092aec80e7b61a61984
gabrielHosino <gabriel.hosino@vtex.com.br>
2021-12-30 09:53:48 -0300

    Add checkout client

--- node/clients/index.ts
+++ node/clients/index.ts
@@ -2,6 +2,8 @@ import { IOClients } from '@vtex/api'
 import { masterDataFor } from '@vtex/clients'
 import type { AffiliatesOrders } from 'vtex.affiliates-commission-service'
 
+import CheckoutExtended from './checkout'
+
 // Extend the default IOClients implementation with our own custom clients.
 export class Clients extends IOClients {
   public get affiliatesOrders() {
@@ -9,5 +11,9 @@ export class Clients extends IOClients {
       'affiliatesOrders',
       masterDataFor<AffiliatesOrders>('affiliatesOrders')
     )
+  }
+
+  public get checkout() {
+    return this.getOrSet('checkout', CheckoutExtended)
   }
 }
