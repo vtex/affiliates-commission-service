86a708703d4f723eb5f8268d8f0b5bb496697958
gabrielHosino <gabriel.hosino@vtex.com.br>
2022-01-03 16:01:08 -0300

    Add logger constants

--- node/utils/constants.ts
+++ node/utils/constants.ts
@@ -1,2 +1,10 @@
 export const CUSTOM_DATA_APP_ID = 'affiliates'
 export const CUSTOM_DATA_FIELD_ID = 'affiliateId'
+
+export const LOGGER_ERROR_METRICS = {
+  updateOrderStatus: 'update-order-status',
+}
+
+export const LOGGER_ERROR_MESSAGES = {
+  updateOrderStatus: 'Error updating the order status',
+}
