d3f8cc6d6448af5d7177ddeea027967dff425c5d
GabrielEluan <gabrieleluan9@gmail.com>
2022-01-21 15:22:09 -0300

    Add unit tests for affiliateOrders resolvers

---
+++ node/__tests__/unit/resolvers/index.ts
@@ -0,0 +1,46 @@
+import type { QueryAffiliateOrdersArgs } from 'vtex.affiliates-commission-service'
+
+import { resolvers } from '../../../resolvers'
+import { parseAffiliateOrdersFilters } from '../../../utils/affiliateOrders'
+
+const {
+  Query: { affiliateOrders },
+} = resolvers
+
+describe('affiliateOrders', () => {
+  const mockedContext = {
+    clients: {
+      affiliatesOrders: {
+        searchRaw: jest.fn(),
+      },
+    },
+  } as unknown as Context
+
+  const mockedArgs = {
+    page: 1,
+    pageSize: 100,
+    filter: {
+      affiliateId: 'mockedAffiliateId',
+    },
+    sorting: {
+      field: 'orderTotal',
+      order: 'DESC',
+    },
+  } as QueryAffiliateOrdersArgs
+
+  it('should call searchRaw with the right arguments', async () => {
+    await affiliateOrders(null, mockedArgs, mockedContext)
+
+    expect(
+      mockedContext.clients.affiliatesOrders.searchRaw
+    ).toHaveBeenCalledWith(
+      {
+        page: mockedArgs.page,
+        pageSize: mockedArgs.pageSize,
+      },
+      ['_all'],
+      'orderTotal DESC',
+      parseAffiliateOrdersFilters({ affiliateId: 'mockedAffiliateId' })
+    )
+  })
+})
