ac85470955b7162ad70346c97d836b1bca6762b1
GabrielEluan <gabrieleluan9@gmail.com>
2022-01-24 10:17:05 -0300

    Add commissionsBySKU resolver

---
+++ node/resolvers/commissionsBySKU/index.ts
@@ -0,0 +1,18 @@
+import type { QueryCommissionsBySkuArgs } from 'vtex.affiliates-commission-service'
+
+import { parseCommissionsBySKUFilters } from '../../utils/filters'
+
+export const queries = {
+  commissionsBySKU: async (
+    _: unknown,
+    { page, pageSize, filter, sorting }: QueryCommissionsBySkuArgs,
+    { clients: { commissionBySKU } }: Context
+  ) => {
+    const pagination = { page, pageSize }
+    const fields = ['_all']
+    const sort = sorting ? `${sorting.field} ${sorting.order}` : undefined
+    const where = filter ? parseCommissionsBySKUFilters(filter) : undefined
+
+    return commissionBySKU.searchRaw(pagination, fields, sort, where)
+  },
+}
--- node/resolvers/index.ts
+++ node/resolvers/index.ts
@@ -2,10 +2,12 @@ import {
   fieldResolvers as affiliateOrdersFieldResolvers,
   queries as affiliateOrdersQueries,
 } from './affiliateOrders'
+import { queries as CommissionsBySKUQueries } from './commissionsBySKU'
 
 export const resolvers = {
   ...affiliateOrdersFieldResolvers,
   Query: {
     ...affiliateOrdersQueries,
+    ...CommissionsBySKUQueries,
   },
 }
