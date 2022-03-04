06eda276d7f95954fc361e9f4dad72086d026b36
GabrielEluan <gabrieleluan9@gmail.com>
2022-03-04 17:22:33 -0300

    Add getLastImportedFile service route and middleware

---
+++ node/middlewares/commission/getLastImportedFile.ts
@@ -0,0 +1,49 @@
+import type { LastImportFileInfo } from '../../services/ImportCommissionsService'
+import { lastImportBucket } from '../../services/ImportCommissionsService'
+
+export async function getLastImportedFile(
+  ctx: Context,
+  next: () => Promise<unknown>
+) {
+  const {
+    vtex: {
+      route: {
+        params: { fileId },
+      },
+    },
+    clients: { vbase },
+  } = ctx
+
+  const { filename } = await vbase.getJSON<LastImportFileInfo>(
+    'last-import',
+    'info'
+  )
+
+  const file = await vbase.getFileStream(lastImportBucket, `${fileId}`)
+
+  if (!file || !filename) {
+    ctx.status = 204
+    ctx.body = {
+      message: 'File not found',
+    }
+
+    return
+  }
+
+  const fileExtension = filename.split('.').pop()
+
+  if (fileExtension === 'csv') {
+    ctx.res.setHeader('Content-Type', 'text/csv')
+  } else if (fileExtension === 'xlsx') {
+    ctx.res.setHeader(
+      'Content-Type',
+      'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    )
+  }
+
+  ctx.res.setHeader('Content-Disposition', `attachment; filename=${filename}`)
+  ctx.body = file
+  ctx.status = 200
+
+  await next()
+}
