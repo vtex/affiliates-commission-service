01335003346718a7793fa368238fbc16f20efeff
GabrielBarros <gabriel.barros@vtex.com.br>
2022-06-17 12:08:59 -0300

    Rule changes

--- node/resolvers/affiliateOrders/totalizerProfileFieldResolver.ts
+++ node/resolvers/affiliateOrders/totalizerProfileFieldResolver.ts
@@ -12,19 +12,6 @@ export const totalizersProfileFieldResolver = async (
 
   const where = filter ? parseAffiliateOrdersFilters(filter) : ''
 
-  if (filter?.status != null) {
-    const allOrders = await affiliatesOrdersAggregate.aggregateValue(
-      'orderTotal',
-      where
-    )
-
-    return {
-      totalCancelled: filter?.status === 'cancelled' ? allOrders : 0,
-      totalApproved: filter?.status === 'payment-approved' ? allOrders : 0,
-      totalInvoiced: filter?.status === 'invoiced' ? allOrders : 0,
-    }
-  }
-
   const whereCancelled = where
     ? `${where} AND status=cancelled`
     : 'status=cancelled'
@@ -33,28 +20,62 @@ export const totalizersProfileFieldResolver = async (
     ? `${where} AND status=payment-approved`
     : 'status=payment-approved'
 
+  const wherePending = where
+    ? `${where} AND status=payment-pending`
+    : 'status=payment-pending'
+
+  const whereCreated = where
+    ? `${where} AND status=order-created`
+    : 'status=order-created'
+
   const whereInvoiced = where
     ? `${where} AND status=invoiced`
     : 'status=invoiced'
-
-  const cancelledValue = await affiliatesOrdersAggregate.aggregateValue(
-    'orderTotal',
-    whereCancelled
-  )
 
   const approvedValue = await affiliatesOrdersAggregate.aggregateValue(
     'orderTotal',
     whereApproved
   )
 
-  const invoicedValue = await affiliatesOrdersAggregate.aggregateValue(
+  const pendingValue = await affiliatesOrdersAggregate.aggregateValue(
     'orderTotal',
-    whereInvoiced
+    wherePending
+  )
+
+  const createdValue = await affiliatesOrdersAggregate.aggregateValue(
+    'orderTotal',
+    whereCreated
+  )
+
+  const onGoingValue =
+    approvedValue.result + pendingValue.result + createdValue.result
+
+  if (filter?.status == null) {
+    const cancelledValue = await affiliatesOrdersAggregate.aggregateValue(
+      'orderTotal',
+      whereCancelled
+    )
+
+    const invoicedValue = await affiliatesOrdersAggregate.aggregateValue(
+      'orderTotal',
+      whereInvoiced
+    )
+
+    return {
+      totalCancelled: cancelledValue?.result,
+      totalApproved: onGoingValue,
+      totalInvoiced: invoicedValue?.result,
+    }
+  }
+
+  const allOrders = await affiliatesOrdersAggregate.aggregateValue(
+    'orderTotal',
+    where
   )
 
   return {
-    totalCancelled: cancelledValue?.result,
-    totalApproved: approvedValue?.result,
-    totalInvoiced: invoicedValue?.result,
+    totalCancelled: filter?.status === 'cancelled' ? allOrders : 0,
+    totalApproved: filter?.status === 'payment-approved' ? onGoingValue : 0,
+    totalInvoiced: filter?.status === 'invoiced' ? allOrders : 0,
   }
 }
