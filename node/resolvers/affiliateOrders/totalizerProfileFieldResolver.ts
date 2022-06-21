07e1b693fe90b405bacc9e1d035fe56395edc67a
GabrielBarros <gabriel.barros@vtex.com.br>
2022-06-21 11:08:14 -0300

    Filter adjustments to work properly with specific statuses

--- node/resolvers/affiliateOrders/totalizerProfileFieldResolver.ts
+++ node/resolvers/affiliateOrders/totalizerProfileFieldResolver.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import type { QueryAffiliateOrdersArgs } from 'vtex.affiliates-commission-service'
 
 import { parseAffiliateOrdersFilters } from '../../utils/filters'
@@ -9,8 +10,67 @@ export const totalizersProfileFieldResolver = async (
   const { filter } = args
   const { affiliatesOrdersAggregate } = ctx.clients
 
-  const where = filter ? parseAffiliateOrdersFilters(filter) : ''
+  if (filter?.status === 'cancel') {
+    delete filter.status
+    console.log(filter)
+    const where = filter ? parseAffiliateOrdersFilters(filter) : ''
+    const whereCanceled = where
+      ? `${where} AND (status=canceled OR status=cancel)`
+      : '(status=canceled OR status=cancel)'
 
+    const canceledValue = await affiliatesOrdersAggregate.aggregateValue(
+      'orderTotal',
+      whereCanceled
+    )
+
+    return {
+      totalCancelled: canceledValue ? canceledValue.result : 0,
+      totalOngoing: 0,
+      totalInvoiced: 0,
+    }
+  }
+
+  if (filter?.status === 'ongoing') {
+    delete filter.status
+    const where = filter ? parseAffiliateOrdersFilters(filter) : ''
+
+    const whereOngoing = where
+      ? `${where} AND (status=payment-approved OR status=payment-pending OR status=on-order-completed)`
+      : '(status=payment-approved OR status=payment-pending OR status=on-order-completed)'
+
+    const onGoingValue = await affiliatesOrdersAggregate.aggregateValue(
+      'orderTotal',
+      whereOngoing
+    )
+
+    return {
+      totalCancelled: 0,
+      totalOngoing: onGoingValue ? onGoingValue.result : 0,
+      totalInvoiced: 0,
+    }
+  }
+
+  if (filter?.status === 'invoiced') {
+    delete filter.status
+    const where = filter ? parseAffiliateOrdersFilters(filter) : ''
+
+    const whereInvoiced = where
+      ? `${where} AND status=invoiced`
+      : 'status=invoiced'
+
+    const invoicedValue = await affiliatesOrdersAggregate.aggregateValue(
+      'orderTotal',
+      whereInvoiced
+    )
+
+    return {
+      totalCancelled: 0,
+      totalOngoing: 0,
+      totalInvoiced: invoicedValue ? invoicedValue.result : 0,
+    }
+  }
+
+  const where = filter ? parseAffiliateOrdersFilters(filter) : ''
   const whereCanceled = where
     ? `${where} AND (status=canceled OR status=cancel)`
     : '(status=canceled OR status=cancel)'
@@ -37,14 +97,6 @@ export const totalizersProfileFieldResolver = async (
     'orderTotal',
     whereInvoiced
   )
-
-  if (filter?.status != null) {
-    return {
-      totalCancelled: filter?.status === 'cancel' ? canceledValue.result : 0,
-      totalOngoing: filter?.status === 'ongoing' ? onGoingValue.result : 0,
-      totalInvoiced: filter?.status === 'invoiced' ? invoicedValue.result : 0,
-    }
-  }
 
   return {
     totalCancelled: canceledValue?.result,
