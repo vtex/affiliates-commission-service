b2b7bcbec24164439e2afa3f806287f6c25d3b72
Matheus Izidio <matheusimeira@gmail.com>
2022-08-03 16:34:23 -0300

    ajust tests

--- node/__tests__/unit/utils/affiliateOrders.ts
+++ node/__tests__/unit/utils/affiliateOrders.ts
@@ -10,14 +10,31 @@ describe('parseAffiliateOrdersFilters', () => {
     },
   }
 
-  it('should return the right filter', () => {
+  it('should return the right filter and the all invoiced status', () => {
     expect(parseAffiliateOrdersFilters(mockedFilter)).toBe(
-      '(affiliateId=mockedAffiliateId) AND status=invoiced AND (createdIn between mockedStartDate AND mockedEndDate)'
+      '(affiliateId=mockedAffiliateId) AND (status=invoiced OR status=invoice) AND (createdIn between mockedStartDate AND mockedEndDate)'
+    )
+  })
+
+  it('should return the right filter and the all cancel status', () => {
+    mockedFilter.status = 'CANCEL'
+
+    expect(parseAffiliateOrdersFilters(mockedFilter)).toBe(
+      '(affiliateId=mockedAffiliateId) AND (status=canceled OR status=cancel) AND (createdIn between mockedStartDate AND mockedEndDate)'
+    )
+  })
+
+  it('should return the right filter and the all ongoing status', () => {
+    mockedFilter.status = 'ONGOING'
+
+    expect(parseAffiliateOrdersFilters(mockedFilter)).toBe(
+      '(affiliateId=mockedAffiliateId) AND (status=payment-approved OR status=payment-pending OR status=on-order-completed) AND (createdIn between mockedStartDate AND mockedEndDate)'
     )
   })
 
   it('should return the filter with no status', () => {
     mockedFilter.status = 'BAD_STATUS'
+
     expect(parseAffiliateOrdersFilters(mockedFilter)).toBe(
       '(affiliateId=mockedAffiliateId) AND (createdIn between mockedStartDate AND mockedEndDate)'
     )
